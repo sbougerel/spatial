bd9e9524035f284933e785fe25dffa60266cc017
Sylvain Bougerel <sylvain.bougerel.devel@gmail.com>
2012-11-05 19:14:33 +0800

    Added includes for the srand() and time() function that were not present in verify.cpp. Somehow it compiled under G++, but cannot under VC10.

--- tests/verify.cpp
+++ tests/verify.cpp
@@ -19,6 +19,9 @@
  *  framework as a static library.
  */
 
+#include <cstdlib> // rand(), srand()
+#include <ctime>
+
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
