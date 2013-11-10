8d1765b32e65eb291e59523ec6de69d53d4369bf
Sylvain Bougerel <sylvain.bougerel.devel@gmail.com>
2013-11-10 17:34:06 +0800

    Modified a bit the example to make performance readings more readable

--- examples/performance/insert_performance.cpp
+++ examples/performance/insert_performance.cpp
@@ -11,86 +11,53 @@
 
 struct point3_type
 {
-  point3_type(double a, double b, double c)
-  { values[0] = a; values[1] = b; values[2] = c; }
+  template <typename Distribution>
+  point3_type(const Distribution& distrib)
+  { values[0] = distrib(); values[1] = distrib(); values[2] = distrib(); }
   double operator [] (std::size_t index) const { return values[index]; }
   double values[3];
 };
 
 struct point9_type
 {
-  point9_type(double a, double b, double c,
-              double d, double e, double f,
-              double g, double h, double i)
+  template <typename Distribution>
+  point9_type(const Distribution& distrib)
   {
-    values[0] = a; values[1] = b; values[2] = c;
-    values[3] = d; values[4] = e; values[5] = f;
-    values[8] = g; values[7] = h; values[8] = i;
+    values[0] = distrib(); values[1] = distrib(); values[2] = distrib();
+    values[3] = distrib(); values[4] = distrib(); values[5] = distrib();
+    values[8] = distrib(); values[7] = distrib(); values[8] = distrib();
   }
   double operator [] (std::size_t index) const { return values[index]; }
   double values[9];
 };
 
-template <typename Distribution>
-void compare_libraries_3
+template <spatial::dimension_type N, typename Point, typename Distribution>
+void compare_libraries
 (std::size_t data_size, const Distribution& distribution)
 {
-  std::vector<point3_type> data;
+  std::vector<Point> data;
   data.reserve(data_size);
   for (int i = 0; i < data_size; ++i)
-    {
-      double a = distribution(), b = distribution(), c = distribution();
-      std::cout << a << ", " << b << ", " << c << std::endl;
-      data.push_back(point3_type(distribution(), distribution(), distribution()));
-    }
-  std::cout << "for 3 dimensions:" << std::endl;
+    data.push_back(Point(distribution));
 
   // Insert into a point_multiset
-  spatial::point_multiset<3, point3_type> first;
+  std::cout << "\t" << N << " dimensions\t"
+            << data_size << " objects\tpoint_multiset:\t" << std::flush;
+  spatial::point_multiset<N, Point> first;
   utils::time_point start = utils::process_timer_now();
   first.insert(data.begin(), data.end());
   utils::time_point stop = utils::process_timer_now();
-  std::cout << "point_multiset insert time: "
-            << (stop - start) << "sec" << std::endl;
+  std::cout << (stop - start) << "sec" << std::endl;
 
   // Insert into an idle_point_multiset
-  spatial::idle_point_multiset<3, point3_type> second;
+  std::cout << "\t" << N << " dimensions\t"
+            << data_size << " objects\tidle_point_multiset:\t" << std::flush;
+  spatial::idle_point_multiset<N, Point> second;
   start = utils::process_timer_now();
   second.insert(data.begin(), data.end());
   second.rebalance();
   stop = utils::process_timer_now();
-  std::cout << "idle_point_multiset insert time: "
-            << (stop - start) << "sec" << std::endl;
-}
-
-template <typename Distribution>
-void compare_libraries_9
-(std::size_t data_size, const Distribution& distribution)
-{
-  std::vector<point9_type> data;
-  data.reserve(data_size);
-  for (int i = 0; i < data_size; ++i)
-    data.push_back(point9_type(distribution(), distribution(), distribution(),
-                               distribution(), distribution(), distribution(),
-                               distribution(), distribution(), distribution()));
-  std::cout << "for 9 dimensions:" << std::endl;
-
-  // Insert into a point_multiset
-  spatial::point_multiset<8, point9_type> first;
-  utils::time_point start = utils::process_timer_now();
-  first.insert(data.begin(), data.end());
-  utils::time_point stop = utils::process_timer_now();
-  std::cout << "point_multiset insert time: "
-            << (stop - start) << "sec" << std::endl;
-
-  // Insert into an idle_point_multiset
-  spatial::idle_point_multiset<8, point9_type> second;
-  start = utils::process_timer_now();
-  second.insert(data.begin(), data.end());
-  second.rebalance();
-  stop = utils::process_timer_now();
-  std::cout << "idle_point_multiset insert time: "
-            << (stop - start) << "sec" << std::endl;
+  std::cout << (stop - start) << "sec" << std::endl;
 }
 
 int main (int argc, char **argv, char **env)
@@ -106,18 +73,26 @@ int main (int argc, char **argv, char **env)
   std::istringstream argbuf(argv[1]);
   std::size_t data_size;
   argbuf >> data_size;
-  std::cout << "Data sample size is " << data_size << " points." << std::endl;
-
   utils::random_engine engine;
+
+  std::cout << "Uniform distribution" << std::endl;
   utils::uniform_double_distribution uniform(engine, -1.0, 1.0);
-  compare_libraries_3(data_size, uniform);
-  compare_libraries_9(data_size, uniform);
+  compare_libraries<3, point3_type, utils::uniform_double_distribution>
+    (data_size, uniform);
+  compare_libraries<9, point9_type, utils::uniform_double_distribution>
+    (data_size, uniform);
 
+  std::cout << "Normal distribution" << std::endl;
   utils::normal_double_distribution normal(engine, -1.0, 1.0);
-  compare_libraries_3(data_size, normal);
-  compare_libraries_9(data_size, normal);
+  compare_libraries<3, point3_type, utils::normal_double_distribution>
+    (data_size, normal);
+  compare_libraries<9, point9_type, utils::normal_double_distribution>
+    (data_size, normal);
 
+  std::cout << "Narrow normal distribution" << std::endl;
   utils::narrow_double_distribution narrow(engine, -1.0, 1.0);
-  compare_libraries_3(data_size, narrow);
-  compare_libraries_9(data_size, narrow);
+  compare_libraries<3, point3_type, utils::narrow_double_distribution>
+    (data_size, narrow);
+  compare_libraries<9, point9_type, utils::narrow_double_distribution>
+    (data_size, narrow);
 }
