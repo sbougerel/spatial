8df6aa9613115518e20d730e506fb3490d6bdaaf
Sylvain Bougerel <sylvain.bougerel.devel@gmail.com>
2015-02-03 09:48:23 +0800

    In examples, point types (point3_type and point9_types) should be constructed from a Distribution explicitely

--- examples/include/point_type.hpp
+++ examples/include/point_type.hpp
@@ -7,7 +7,7 @@ struct point3_type
   typedef double value_type;
   point3_type() { }
   template <typename Distribution>
-  point3_type(const Distribution& distrib)
+  explicit point3_type(const Distribution& distrib)
   { values[0] = distrib(); values[1] = distrib(); values[2] = distrib(); }
   double operator [] (std::size_t index) const { return values[index]; }
   double values[3];
@@ -18,7 +18,7 @@ struct point9_type
   typedef double value_type;
   point9_type() { }
   template <typename Distribution>
-  point9_type(const Distribution& distrib)
+  explicit point9_type(const Distribution& distrib)
   {
     values[0] = distrib(); values[1] = distrib(); values[2] = distrib();
     values[3] = distrib(); values[4] = distrib(); values[5] = distrib();
