b4554900bd4561d5116c14fa562ca131a4c53cd6
Sylvain Bougerel <sylvain.bougerel.devel@gmail.com>
2015-02-08 23:34:16 +0800

    Adding the performance test for region iterators.

--- examples/include/point_type.hpp
+++ examples/include/point_type.hpp
@@ -2,10 +2,14 @@
 #ifndef SPATIAL_EXAMPLE_POINT_TYPE_HPP
 #define SPATIAL_EXAMPLE_POINT_TYPE_HPP
 
+#include <algorithm> // std::fill
+
 struct point3_type
 {
   typedef double value_type;
   point3_type() { }
+  explicit point3_type (double value)
+  { std::fill(values, values + 9, value); }
   template <typename Distribution>
   explicit point3_type(const Distribution& distrib)
   { values[0] = distrib(); values[1] = distrib(); values[2] = distrib(); }
@@ -17,6 +21,8 @@ struct point9_type
 {
   typedef double value_type;
   point9_type() { }
+  explicit point9_type (double value)
+  { std::fill(values, values + 9, value); }
   template <typename Distribution>
   explicit point9_type(const Distribution& distrib)
   {
---
+++ examples/performance/region_performance.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include <spatial/point_multiset.hpp>
+#include <spatial/idle_point_multiset.hpp>
+#include <spatial/region_iterator.hpp>
+
+#include "../include/chrono.hpp"
+#include "../include/random.hpp"
+#include "../include/point_type.hpp"
+
+template <spatial::dimension_type N, typename Point, typename Distribution>
+void compare_libraries
+(std::size_t data_size, const Distribution& distribution)
+{
+  std::cout << "\t" << N << " dimensions, " << data_size << " objects:" << std::endl;
+  std::vector<Point> data;
+  data.reserve(data_size);
+  Point p0(-1.0), p1(1.0);
+  for (std::size_t i = 0; i < data_size; ++i)
+    data.push_back(Point(distribution));
+  {
+    std::cout << "\t\tidle_point_multiset:\t" << std::flush;
+    spatial::idle_point_multiset<N, Point> cobaye;
+    cobaye.insert_rebalance(data.begin(), data.end());
+    utils::time_point start = utils::process_timer_now();
+    for (spatial::region_iterator<spatial::idle_point_multiset<N, Point> >
+           i = region_begin(cobaye, p0, p1); i != region_end(cobaye, p0, p1); ++i);
+    utils::time_point stop = utils::process_timer_now();
+    std::cout << (stop - start) << "sec" << std::endl;
+    std::cout << "\t\tidle_point_multiset (reverse):\t" << std::flush;
+    start = utils::process_timer_now();
+    spatial::region_iterator<spatial::idle_point_multiset<N, Point> >
+      i = region_end(cobaye, p0, p1), end = region_begin(cobaye, p0, p1);
+    for (; i != end; --i);
+    stop = utils::process_timer_now();
+    std::cout << (stop - start) << "sec" << std::endl;
+  }
+  {
+    std::cout << "\t\tpoint_multiset:\t" << std::flush;
+    spatial::point_multiset<N, Point> cobaye;
+    cobaye.insert(data.begin(), data.end());
+    utils::time_point start = utils::process_timer_now();
+    for (spatial::region_iterator<spatial::point_multiset<N, Point> >
+           i = region_begin(cobaye, p0, p1); i != region_end(cobaye, p0, p1); ++i);
+    utils::time_point stop = utils::process_timer_now();
+    std::cout << (stop - start) << "sec" << std::endl;
+    std::cout << "\t\tpoint_multiset (reverse):\t" << std::flush;
+    start = utils::process_timer_now();
+    spatial::region_iterator<spatial::point_multiset<N, Point> >
+      i = region_end(cobaye, p0, p1), end = region_begin(cobaye, p0, p1);
+    for (; i != end; --i);
+    stop = utils::process_timer_now();
+    std::cout << (stop - start) << "sec" << std::endl;
+  }
+}
+
+int main (int argc, char **argv)
+{
+  if (argc != 2)
+    {
+      std::cerr << "Usage: " << argv[0] << " <sample size: integer>"
+                << std::endl;
+      return 1;
+    }
+
+  std::istringstream argbuf(argv[1]);
+  std::size_t data_size;
+  argbuf >> data_size;
+  utils::random_engine engine;
+
+  std::cout << "Uniform distribution:" << std::endl;
+  utils::uniform_double_distribution uniform(engine, -1.0, 1.0);
+  compare_libraries<3, point3_type, utils::uniform_double_distribution>
+    (data_size, uniform);
+  compare_libraries<9, point9_type, utils::uniform_double_distribution>
+    (data_size, uniform);
+
+  std::cout << "Normal distribution:" << std::endl;
+  utils::normal_double_distribution normal(engine, -1.0, 1.0);
+  compare_libraries<3, point3_type, utils::normal_double_distribution>
+    (data_size, normal);
+  compare_libraries<9, point9_type, utils::normal_double_distribution>
+    (data_size, normal);
+
+  std::cout << "Narrow normal distribution:" << std::endl;
+  utils::narrow_double_distribution narrow(engine, -1.0, 1.0);
+  compare_libraries<3, point3_type, utils::narrow_double_distribution>
+    (data_size, narrow);
+  compare_libraries<9, point9_type, utils::narrow_double_distribution>
+    (data_size, narrow);
+}
